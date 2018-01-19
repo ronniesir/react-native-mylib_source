94661fc2e761a232b24d481eb1dd1a5ecfe4588d
ronnie.jing <ronnie.jing@gtedx.com>
2018-01-19 14:36:19 +0800

    init

---
+++ android/app/src/main/java/com/ronnie/reactnativelibsource/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ronnie.reactnativelibsource;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ android/react-native-ronnie-lib/src/main/java/com/ronnie/react_native_ronnie_lib/RonnieLibPackage.java
@@ -0,0 +1,8 @@
+package com.ronnie.react_native_ronnie_lib;
+
+/**
+ * Created by ronnie on 2018/1/19.
+ */
+
+public class RonnieLibPackage {
+}
